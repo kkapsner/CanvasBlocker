040acb311f6f8e086ee6ed6a700c39d064cd0c7d
kkapsner <github@mail.kkapsner.de>
2018-09-12 22:54:25 +0200

    Added missing translation checker

---
+++ .tools/checkMissingTranslation.js
@@ -0,0 +1,15 @@
+const en = require("../_locales/en/messages.json");
+const enKeys = Object.keys(en);
+
+const language = process.argv[2];
+
+const la = require("../_locales/" + language + "/messages.json");
+const laKeys = Object.keys(la);
+
+enKeys.forEach(function(key){
+	if (en[key].message){
+		if (!la[key] || !la[key].message){
+			console.log(key, "missing");
+		}
+	}
+});
