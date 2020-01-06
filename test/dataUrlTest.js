16fb0423357c9aeca61f62db91ee72d04733ebcb
kkapsner <github@mail.kkapsner.de>
2020-01-06 14:55:11 +0100

    Fixed origin display in blobIframe

--- test/dataUrlTest.js
+++ test/dataUrlTest.js
@@ -7,7 +7,10 @@
 	
 	document.getElementById("code").textContent = iframeCode;
 	
-	const blob = new Blob([iframeCode], {type: "text/html"});
+	const blob = new Blob([iframeCode.replace(
+		"const origin = \"data URL iframe\";",
+		"const origin = \"blob iframe\";"
+	)], {type: "text/html"});
 	const newurl = window.URL.createObjectURL(blob);
 	document.getElementById("blobIframe").src = newurl;
 }());
